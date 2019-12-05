483393a491a63d21adc5bf8a919c8fe391a5fed4
JulianThoms <julian.thoms@st.oth-regensburg.de>
2019-12-05 14:12:24 +0100

    adding more PUG and fixing bootstrap, changed nav bar

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require("express");
 var pg = require("pg");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+
 let loggedIn = false;
-var CON_STRING = process.env.DB_CON_STRING;
+
+const CON_STRING = process.env.DB_CON_STRING;
 if (CON_STRING == undefined) {
     console.log("Error: Environment variable DB_CON_STRING not set!");
     process.exit(1);
@@ -28,6 +30,7 @@ app.use(session({
     saveUninitialized: false
 }));
 
+
 //allowing CSS
 app.use(express.static(__dirname + '/public'));
 
@@ -38,36 +41,56 @@ app.get("/", function (req, res) {
   req.session.loggedIn = true;
   res.render("index", {loggedIn});
 });
+
 app.get("/login", function(req, res){
   res.render("login");
 });
+
 app.get("/signup", function(req, res){
   res.render("signup");
 });
+
 app.get("/successful_login", function(req, res){
-  res.render("successful_login");
+  if(req.session.user != undefined){
+    res.render("successful_login");
+  }
+  else{
+    res.render("login", {error_login: "Please log in to access this Page!"})
+  }
 });
 
+
+
 app.post("/signup", urlencodedParser, function(req, res){
-  var username = req.body.username;
-  var userpassword = req.body.password;
+  const username = req.body.username;
+  const userpassword = req.body.password;
   dbClient.query("INSERT INTO users (name, password) VALUES ($1, $2)", [username, userpassword], function(dbErr, dbRes){});
   res.redirect("/");
 });
 
 app.post("/login", urlencodedParser, function(req, res){
-  var username = req.body.username;
-  var userpassword = req.body.password;
+  const username = req.body.username;
+  const userpassword = req.body.password;
 
   dbClient.query("SELECT * FROM users WHERE name = $1 AND password = $2", [username, userpassword], function(dbErr, dbRes){
     if(dbRes.rows.length == 0){
       res.render("login", {error_login: "Username or Password wrong!"})
     }
     else{
+      req.session.user = username;
       res.redirect("/successful_login");
     }
-
   });
+});
+
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.send("logout success!");
+});
+
+app.post("/search", urlencodedParser, function(req, res){
+  const search = req.body.searchTerm;
+  dbClient.query("SELECT * FROM books WHERE ")
 });
 
 app.listen(PORT, function () {
