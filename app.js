33b9977366f3179caaa5c049b2bc3c64a6d8e913
JulianThoms <julian.thoms@st.oth-regensburg.de>
2019-12-07 16:41:16 +0100

    Added: Reading reviews! Todo: Writing reviews

--- app.js
+++ app.js
@@ -170,11 +170,13 @@ app.get("/favourites", function (req, res){
     if(id != undefined){
       dbClient.query("SELECT books.title FROM (books INNER JOIN users_favourites ON books.id_book = users_favourites.id_book) INNER JOIN users ON users.id_user = users_favourites.id_user WHERE users.id_user = $1 LIMIT 50", [id], function(dbErr, dbRes){
         res.render("favourites", {
-          favourites: dbRes.rows, username
+          favourites: dbRes.rows,
+          username
         });
       });
-    }});
+    }
   });
+});
 
 app.post("/search", urlencodedParser, function(req, res){
   let username = req.session.user;
@@ -202,40 +204,59 @@ app.post("/search", urlencodedParser, function(req, res){
   }
 });
 
-app.get("/search/:id", function (req, res) {
-    /* List details about a single flight */
-    var bookID = req.params.id;
 
-    dbClient.query("SELECT * FROM books WHERE id_book=$1", [bookID], function (dbErr, dbRes) {
-      if (dbErr != undefined){
-        res.render("error", {error_message: "Error, please try again!"});
-      } else
-        if (dbRes.rows.length == 0) {
-            res.render("error", {
-                error_message: "This book does not exist!"
-            });
-          }
-        else{
-          res.render("book_closeup", {
-              title: dbRes.rows[0].title,
-              year: dbRes.rows[0].year,
-              isbn: dbRes.rows[0].isbn,
-              author: dbRes.rows[0].author
+app.get("/search/:id", function (req, res) {
+  /* List details about a single flight */
+  const bookID = req.params.id;
+  const userName = req.session.user;
+  let userID;
+  dbClient.query("SELECT id_user FROM users WHERE name=$1", [userName], function (dbErr, dbRes) {
+    userID = dbRes.rows[0].id_user;
+  })
+  let reviews;
+  let no_reviews = false;
+  dbClient.query("SELECT * FROM books WHERE id_book=$1", [bookID], function (dbErr, dbRes) {
+    if (dbErr != undefined){
+      res.render("error", {error_message: "Error, please try again!"});
+    } else
+    if (dbRes.rows.length == 0) {
+      res.render("error", {
+        error_message: "This book does not exist!"
+      });
+    }
+    else{
+      dbClient.query("SELECT users_reviews.review, users.name FROM users_reviews INNER JOIN users ON users_reviews.id_user = users.id_user WHERE users_reviews.id_book=$1 AND users.id_user=$2", [bookID, userID], function (dbErrReview, dbResReview) {
+        console.log(dbResReview)
+        if(dbResReview.rows.length == 0){
+          no_reviews = true;
         }
-      )}
-    });
+        else{
+          reviews = dbResReview.rows;
+        }
+        res.render("book_closeup", {
+          title: dbRes.rows[0].title,
+          year: dbRes.rows[0].year,
+          isbn: dbRes.rows[0].isbn,
+          author: dbRes.rows[0].author,
+          id_book: dbRes.rows[0].id_book,
+          reviews,
+          no_reviews: no_reviews
+        });
+      });
+    }
+  });
+});
+
+  app.get("/secret", function(req, res){
+    res.render("secret");
+  })
+
+  app.listen(PORT, function () {
+    console.log(`Shopping App listening on Port ${PORT}`);
   });
 
-app.get("/secret", function(req, res){
-  res.render("secret");
-})
 
-app.listen(PORT, function () {
-  console.log(`Shopping App listening on Port ${PORT}`);
-});
-
-
-//FIXIT
-app.get("*", function(req, res){
-  res.render("error", {error_message: "This page does not exist!"})
-});
+  //FIXIT
+  app.get("*", function(req, res){
+    res.render("error", {error_message: "This page does not exist!"})
+  });
